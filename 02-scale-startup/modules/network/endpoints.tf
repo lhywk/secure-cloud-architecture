730006fc3fef671e9c7833492e08aeffec94715e
alstnuu2 <alstnuu2@naver.com>
2026-04-07 21:51:45 +0900

    fix: S3 Endpoint deprecated 속성 수정

--- 02-scale-startup/modules/network/endpoints.tf
+++ 02-scale-startup/modules/network/endpoints.tf
@@ -2,7 +2,7 @@
 # Routes EC2 → S3 traffic through the AWS internal network (no NAT, no internet)
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  service_name      = "com.amazonaws.${data.aws_region.current.id}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.public.id]
 
