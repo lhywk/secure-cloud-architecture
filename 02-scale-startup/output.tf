c113904fc70399e9ed65004cbacf0ba85c110901
alstnuu2 <alstnuu2@naver.com>
2026-04-11 03:17:12 +0900

    feat: 루트 output에 애플리케이션 URL과 CloudFront 도메인 추가

---
+++ 02-scale-startup/output.tf
@@ -0,0 +1,9 @@
+output "application_url" {
+  description = "Primary application URL"
+  value       = "https://${var.subdomain}.${var.domain_name}"
+}
+
+output "cloudfront_domain_name" {
+  description = "CloudFront distribution domain name"
+  value       = module.cdn.cloudfront_domain_name
+}
