96587b78d66774236e4cd54ab38ec1aad55abea9
alstnuu2 <alstnuu2@naver.com>
2026-04-11 19:24:57 +0900

    fix: 알람 지표 11개 -> 10개로 수정

--- 02-scale-startup/modules/observability/cloudwatch.tf
+++ 02-scale-startup/modules/observability/cloudwatch.tf
@@ -298,28 +298,6 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
   tags = local.common_tags
 }
 
-resource "aws_cloudwatch_metric_alarm" "alb_target_5xx" {
-  alarm_name          = "${var.project}-${var.environment}-alb-target-5xx"
-  alarm_description   = "5분 내 Target 5xx 20회 이상. 백엔드 애플리케이션 또는 인스턴스 장애 가능성. ALB Access Log와 애플리케이션 로그를 함께 확인."
-  namespace           = "AWS/ApplicationELB"
-  metric_name         = "HTTPCode_Target_5XX_Count"
-  statistic           = "Sum"
-  period              = 300
-  evaluation_periods  = 2
-  threshold           = 20
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  treat_missing_data  = "notBreaching"
-
-  dimensions = {
-    LoadBalancer = local.alb_arn_suffix
-  }
-
-  alarm_actions = [aws_sns_topic.alarm.arn]
-  ok_actions    = [aws_sns_topic.alarm.arn]
-
-  tags = local.common_tags
-}
-
 # EC2-based alarms
 
 # Detect EC2 CPU overload (e.g., cryptojacking)
